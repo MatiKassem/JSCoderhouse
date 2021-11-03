36c91b1ad8276561b441a2c566f94f798c4a5785
Matias Kassem <positivo.matias.kassem@gmail.com>
2021-11-03 12:58:41 -0300

    segundo desafío del curso

---
+++ clase2Kassem.js
@@ -0,0 +1,21 @@
+let nombreUsuario = prompt("Ingrese su nombre completo");
+let edadUsuario = prompt("Ingrese su edad");
+parseInt(edadUsuario);
+
+
+if (edadUsuario > 20 && edadUsuario <= 24){
+    alert("Bienvenido " + nombreUsuario + " al torneo Nacional Master Argentino de natación su categoría es PRE MASTER")
+}
+else if(edadUsuario > 25 && edadUsuario <=29){
+    alert("Bienvenido "+ nombreUsuario +" al torneo Nacional Master Argentino de natación su categoría es MASTER A")
+}
+else if(edadUsuario > 30 && edadUsuario <=34){
+    alert("Bienvenido "+ nombreUsuario +" al torneo Nacional Master Argentino de natación su categoría es MASTER B")
+}
+else if(edadUsuario > 35 && edadUsuario <=39){
+    alert("Bienvenido "+ nombreUsuario +" al torneo Nacional Master Argentino de natación su categoría es MASTER C")
+}
+else{
+    alert("Bienvenido "+ nombreUsuario +" al torneo Nacional Master Argentino de natación su categoría es LIBRE")
+};
+
